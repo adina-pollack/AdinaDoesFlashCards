43966f9c7849b6d9414dd784de4af71d518e0bbc
Adina Pollack <adinap16@gmail.com>
2016-06-08 16:19:55 -0400

    added CSS

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
   var currentCardIndex = 0
   var flashCards = $(".flashCards");
   var seconds = 0;
+  var minutes = 0;
   var start = $("#start");
   var userGuess;
-  var arrowKeys = 0;
+  var numCorrect = 0;
+  var gameHasStarted = false;
   var cards = [
     {
       question:"What is 4 + 5?",
@@ -23,7 +25,7 @@ $(document).ready(function(){
       answer:"56"
     },
     {
-      question:"Find the derivative of y = 2x^3 - 4x^2 + 3x - 5",
+      question:"Find the derivative of <br>y = 2x^3 - 4x^2 + 3x - 5",
       answer:"6x^2-8x+3"
     }
   ];
@@ -33,35 +35,43 @@ $(document).ready(function(){
   }
 
   function displayObjectOne(){
+    currentCardIndex = 0;
     $(".flashCard").html(cards[0].question);
   }
 
   function updateTime(){
     if (cards.length > 0){
+      if (seconds < 59){
         seconds++;
-    }
-    $("#timer").text(seconds);
+      }
+      else if (seconds >= 59){
+        seconds = 0
+        minutes += 1;
+
+      }
+    };
+    $("#timer").text("Time: " + minutes + " " + "m" + " : " + seconds + " " + "s");
   }
 
   function startTimer(){
     timerId = window.setInterval(updateTime, 1000);
   }
 
+  function updateCorrect(){
+    $("#scoreBoard").text("Correct: " + numCorrect);
+  }
+
   function clearInput(){
-    $(".input").val(" ");
+    $(".input").val("");
   }
 
   $(document).on("keydown", function test(e){
-    // var arrowKeys = 0;
+    if (gameHasStarted) return;
     if (e.keyCode == '39'){ // Right arrow
       if (currentCardIndex < cards.length - 1){
         currentCardIndex += 1;
         displayQuestion();
       }
-      // else if (arrowKeys == 1){
-      //   currentCardIndex;
-      //   return;
-      // }
       else{
         return false;
       }
@@ -70,19 +80,11 @@ $(document).ready(function(){
         currentCardIndex -= 1;
         displayQuestion();
       }
-      // else if (arrowKeys == 1){
-      //   currentCardIndex;
-      //   return;
-      // }
       else{
         return false;
       }
     }
-    start.on("click", function(){
-
-      arrowKeys = 1;
     });
-  });
 
   displayQuestion();
 
@@ -94,23 +96,28 @@ $(document).ready(function(){
     $(".userInput").css("visibility", "visible");
     $("#timer").css("visibility", "visible");
     $("#revealButton").css("visibility", "hidden");
+    $("#scoreBoard").css("visibility", "visible");
     displayObjectOne();
+    startTimer();
+    gameHasStarted = true;
   });
-
-  start.on("click", startTimer);
 
   $("#guess").on("click", function(){
     var userGuess = $(".input").val();
     if (userGuess == cards[currentCardIndex].answer){
       if (currentCardIndex < cards.length - 1){
+        // $("#flashCard").css("background-color", "#00ff00");
         alert("Correct!");
         currentCardIndex += 1;
         displayQuestion();
         clearInput();
+        numCorrect += 1;
+        updateCorrect();
       }
       else if (currentCardIndex == cards.length - 1){
         alert("Correct! Game over!");
         window.clearInterval(timerId);
+        clearInput();
       }
     }
     else{
@@ -121,6 +128,4 @@ $(document).ready(function(){
       clearInput();
     }
   });
-
-
 });
